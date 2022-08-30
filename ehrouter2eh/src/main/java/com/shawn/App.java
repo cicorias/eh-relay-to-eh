6bda83d2deed0dc8697ecd34a20526b0359691d7
Shawn Cicoria <github@cicoria.com>
2022-08-29 20:54:40 -0400

    update

--- ehrouter2eh/src/main/java/com/shawn/App.java
+++ ehrouter2eh/src/main/java/com/shawn/App.java
@@ -1,9 +1,12 @@
 package com.shawn;
+
 // finish me https://docs.microsoft.com/en-us/azure/event-hubs/event-hubs-java-get-started-send
 import com.azure.messaging.eventhubs.*;
 import com.azure.messaging.eventhubs.checkpointstore.blob.BlobCheckpointStore;
 import com.azure.messaging.eventhubs.models.*;
 import com.azure.storage.blob.*;
+
+import java.io.IOException;
 import java.util.function.Consumer;
 
 import picocli.CommandLine;
@@ -16,14 +19,72 @@ import org.apache.logging.log4j.LogManager;
 @Command(name = "App", version = "App 1.0-SNAPSHOT", mixinStandardHelpOptions = true)
 public class App implements Runnable {
 
+    private static final String connectionString = "<Event Hubs namespace connection string>";
+    private static final String eventHubName = "<Event hub name>";
+    private static final String storageConnectionString = "<Storage connection string>";
+    private static final String storageContainerName = "<Storage container name>";
+
     private static final Logger LOGGER = LogManager.getLogger(App.class);
 
-    @Option(names = { "-p", "--port" }, description = "port to listen on") 
+    @Option(names = { "-p", "--port" }, description = "port to listen on")
     int port = 8080;
+
+    public static final Consumer<EventContext> PARTITION_PROCESSOR = eventContext -> {
+        PartitionContext partitionContext = eventContext.getPartitionContext();
+        EventData eventData = eventContext.getEventData();
+
+        System.out.printf("Processing event from partition %s with sequence number %d with body: %s%n",
+                partitionContext.getPartitionId(), eventData.getSequenceNumber(), eventData.getBodyAsString());
+
+        // Every 10 events received, it will update the checkpoint stored in Azure Blob
+        // Storage.
+        if (eventData.getSequenceNumber() % 10 == 0) {
+            eventContext.updateCheckpoint();
+        }
+    };
+
+    public static final Consumer<ErrorContext> ERROR_HANDLER = errorContext -> {
+        System.out.printf("Error occurred in partition processor for partition %s, %s.%n",
+                errorContext.getPartitionContext().getPartitionId(),
+                errorContext.getThrowable());
+    };
 
     @Override
     public void run() {
         LOGGER.info(String.format("Hello, my port is %d!", port));
+        BlobContainerAsyncClient blobContainerAsyncClient = new BlobContainerClientBuilder()
+                .connectionString(storageConnectionString)
+                .containerName(storageContainerName)
+                .buildAsyncClient();
+
+        // Create a builder object that you will use later to build an event processor
+        // client to receive and process events and errors.
+        EventProcessorClientBuilder eventProcessorClientBuilder = new EventProcessorClientBuilder()
+                .connectionString(connectionString, eventHubName)
+                .consumerGroup(EventHubClientBuilder.DEFAULT_CONSUMER_GROUP_NAME)
+                .processEvent(PARTITION_PROCESSOR)
+                .processError(ERROR_HANDLER)
+                .checkpointStore(new BlobCheckpointStore(blobContainerAsyncClient));
+
+        // Use the builder object to create an event processor client
+        EventProcessorClient eventProcessorClient = eventProcessorClientBuilder.buildEventProcessorClient();
+
+        System.out.println("Starting event processor");
+        eventProcessorClient.start();
+
+        System.out.println("Press enter to stop.");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("Stopping event processor");
+        eventProcessorClient.stop();
+        System.out.println("Event processor stopped.");
+
+        System.out.println("Exiting process");
     }
 
     public static void main(String[] args) {
